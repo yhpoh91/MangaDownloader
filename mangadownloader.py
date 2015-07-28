10effdb8d59d2ce685f8a0549ead3e8949b5409b
Yee Hui Poh <yhpoh91@gmail.com>
2015-07-28 17:43:53 +0800

    Added directory and file check, reversed download order

--- mangadownloader.py
+++ mangadownloader.py
@@ -9,25 +9,38 @@ def downloadManga(url):
 
 	# Create Manga Directory
 	title = getPossibleFileName(mangaInfo['title'])
-	os.mkdir(title)
+	if(not os.path.exists(title)):
+		os.mkdir(title)
 
 	# Change to Manga Directory
 	os.chdir(title)
-	
+	chapters = []
 	for chapter in mangaInfo['chapters']:
+		chapters.insert(0, chapter)
+
+	for chapter in chapters:
 		# Create Chapter Directory
 		name = getPossibleFileName(chapter['name'])
-		os.mkdir(name)
+		if(not os.path.exists(name)):
+			os.mkdir(name)
 
 		# Change to Chapter Directory
 		os.chdir(name)
 		
-		# Get Chapter Images URLs
-		chapterURL = chapter['url']
-		chapterImageURLs = getChapterImages(chapterURL)
-		
-		# Download Images
-		downloadChapter(chapterImageURLs)
+		if(not os.path.exists('.mangadone')):
+			# Get Chapter Images URLs
+			chapterURL = chapter['url']
+			chapterImageURLs = getChapterImages(chapterURL)
+			
+			# Download Images
+			downloadChapter(chapterImageURLs)
+
+			# Check number of Images
+			numberOfImages = len(chapterImageURLs)
+			numberOfDownloadedFiles = len([filename for filename in os.listdir('.') if os.path.isfile(filename)])
+			if(numberOfImages == numberOfDownloadedFiles):
+				# Create Done File
+				open('.mangadone', 'a').close()
 		
 		# Return to Parent Directory
 		os.chdir("..")
@@ -48,18 +61,26 @@ def getPossibleFileName(name):
 
 def downloadChapter(imageURLs):
 	for line in imageURLs:
-		co("curl -O -J \"" + line + "\"", shell=True)
+		try:
+			co("curl -O -J \"" + line + "\"", shell=True)
+		except Exception, e:
+			print str(e)
+			pass
 	pass
 
 def getChapterImages(url):
 	imageURLsReturn = []
-	response = co("curl \"" + url + "\"", shell=True)
-	responseDataSplit = response.split('\n')
+	try:
+		response = co("curl \"" + url + "\"", shell=True)
+		responseDataSplit = response.split('\n')
 
-	for line in responseDataSplit:
-		imageURL = re.findall("\s+lstImages.push\(\"(.+)\"\);", line)
-		if(len(imageURL) > 0):
-			imageURLsReturn.append(imageURL[0])
+		for line in responseDataSplit:
+			imageURL = re.findall("\s+lstImages.push\(\"(.+)\"\);", line)
+			if(len(imageURL) > 0):
+				imageURLsReturn.append(imageURL[0])
+	except Exception, e:
+		print str(e)
+		pass
 
 	return imageURLsReturn
 
@@ -67,17 +88,22 @@ def getMangaInfo(url):
 	chapterURLs = []
 	chapterURLsReturn = []
 	mangaTitle = "gg"
-	response = co("curl \"" + url + "\"", shell=True)
-	responseDataSplit = response.split('\n')
+	try:
+		response = co("curl \"" + url + "\"", shell=True)
+		responseDataSplit = response.split('\n')
 
-	for line in responseDataSplit:
-		mangaTitles = re.findall("<a Class=\"bigChar\" href=\"(.+)\">(.+)</a>", line)
-		if(len(mangaTitles) > 0):\
-			mangaTitle = mangaTitles[0][1]
+		for line in responseDataSplit:
+			mangaTitles = re.findall("<a Class=\"bigChar\" href=\"(.+)\">(.+)</a>", line)
+			if(len(mangaTitles) > 0):\
+				mangaTitle = mangaTitles[0][1]
 
-		chapterURL = re.findall("<a href=\"(.+)\" title=\"Read (.+) online\">", line)
-		if(len(chapterURL) > 0):
-			chapterURLsReturn.append({"url":"http://kissmanga.com" + str(chapterURL[0][0]), "name":str(chapterURL[0][1])})
+			chapterURL = re.findall("<a href=\"(.+)\" title=\"Read (.+) online\">", line)
+			if(len(chapterURL) > 0):
+				chapterURLsReturn.append({"url":"http://kissmanga.com" + str(chapterURL[0][0]), "name":str(chapterURL[0][1])})
+	
+	except Exception, e:
+		print str(e)
+		pass
 
 	mangaInfo = {"title":mangaTitle, "chapters":chapterURLsReturn}
 	return mangaInfo
